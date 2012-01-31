f3ddffc542b92a46c903245db186b34f5d0f9126
Owen Watson <owen1.watson@gmail.com>
2012-01-31 01:34:54 -0500

    fixing setup

--- project/createNiKinectXMLFile.cpp
+++ project/createNiKinectXMLFile.cpp
@@ -10,13 +10,13 @@ int main()
 	XMLNode serial = camera.addChild("serial");
 	serial.addAttribute("serial","908h98hf9a8hdf98dh");
 	XMLNode calibration = camera.addChild("calibration");
-	calibration.addAttribute("location","/data/config/calibration.yml");
+	calibration.addAttribute("location","/data/cameraCalibration/calibration.yml");
 	XMLNode offset = camera.addChild("offset");
 	offset.addAttribute("x", "0.5");
 	offset.addAttribute("y", "0.5");
 	offset.addAttribute("z", "0.5");
 	XMLNode rotation = camera.addChild("rotation");
 	rotation.addAttribute("rotation", "0.5");
-  	std::cout<< camera.writeToFile("../../data/config/test.xml");  
+  	std::cout<< camera.writeToFile("../../data/cameraConfig/test.xml");  
 	
 }
